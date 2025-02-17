32b5de2abd198b197a3170bfafcfe5d0e06b6f81
Madhesh H <144439699+madddx@users.noreply.github.com>
2025-02-17 07:56:18 +0530

    Create 1079. Letter Tile Possibilities.cpp

---
+++ 1001-1100/1079. Letter Tile Possibilities.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+void backtrack(unordered_map<char,int>&mapi,int &count){
+    for(auto&entry:mapi){
+    if(entry.second>0){
+        entry.second--;
+        count++;
+        backtrack(mapi,count);
+        entry.second++;
+    }
+    }
+}
+    int numTilePossibilities(string tiles) {
+        unordered_map<char,int>mapi;
+        for(int i=0;i<tiles.size();i++){
+            mapi[tiles[i]]++;
+        }
+        int count=0;
+         backtrack(mapi,count);
+         return count;
+    }
+};

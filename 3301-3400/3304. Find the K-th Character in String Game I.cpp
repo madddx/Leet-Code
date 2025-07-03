c79a9d612ffc76738da23da0552be774360cd171
Madhesh H <144439699+madddx@users.noreply.github.com>
2025-07-03 19:42:01 +0530

    Create 3304. Find the K-th Character in String Game I.cpp

---
+++ 3301-3400/3304. Find the K-th Character in String Game I.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int count_ones(int n) {
+        int result = 0;
+        while (n) {
+            n &= n - 1;
+            ++result;
+        }
+        return result;
+    }
+
+    char kthCharacter(int k) {
+        return 'a' + count_ones(k - 1);
+    }
+};

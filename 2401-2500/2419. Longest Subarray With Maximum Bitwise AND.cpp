18303c8c82fc2991a4946781212757354bf07d84
Madhesh H <144439699+madddx@users.noreply.github.com>
2025-07-30 10:11:59 +0530

    Update 2419. Longest Subarray With Maximum Bitwise AND.cpp

--- 2401-2500/2419. Longest Subarray With Maximum Bitwise AND.cpp
+++ 2401-2500/2419. Longest Subarray With Maximum Bitwise AND.cpp
@@ -1,35 +1,13 @@
-#include <vector>
-#include <algorithm>
-
 class Solution {
 public:
-    int longestSubarray(std::vector<int>& nums) {
-        if (nums.empty()) {
-            return 0;
+    int longestSubarray(vector<int>& nums) {
+        int maxi = *max_element(nums.begin(), nums.end());
+        int cons = 1, res = 1;
+        for (int i = 1; i < nums.size(); i++) {
+            if (nums[i] == nums[i - 1] && nums[i] == maxi) cons++;
+            else cons = 1;
+            res = max(res, cons);
         }
-
-        // 1. Find the maximum value in the array.
-        int maxVal = 0;
-        for (int num : nums) {
-            if (num > maxVal) {
-                maxVal = num;
-            }
-        }
-        
-        // 2. Find the length of the longest subarray of maxVal.
-        int maxLen = 0;
-        int currentLen = 0;
-        
-        for (int num : nums) {
-            if (num == maxVal) {
-                currentLen++;
-            } else {
-                maxLen = std::max(maxLen, currentLen);
-                currentLen = 0;
-            }
-        }
-        
-        // 3. Final check for a trailing streak.
-        return std::max(maxLen, currentLen);
+        return res;
     }
 };

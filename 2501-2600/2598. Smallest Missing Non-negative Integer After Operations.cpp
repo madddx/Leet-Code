42fe988e7e4ad6650565e0dfbe80b02736b3c69b
Madhesh H <144439699+madddx@users.noreply.github.com>
2025-10-16 09:11:43 +0530

    Create 2598. Smallest Missing Non-negative Integer After Operations.cpp

---
+++ 2501-2600/2598. Smallest Missing Non-negative Integer After Operations.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+
+    int findSmallestInteger(vector<int>& nums, int value) {
+        vector<int> rems(value + 1, 0);
+        int ret = 0;
+        for (int i = 0; i < nums.size(); ++i) {
+            if (nums[i] >= 0)
+                ++rems[nums[i] % value];
+            else
+                ++rems[value + (nums[i] % value)];
+        }
+        rems[0] += rems[value];
+        while(rems[ret % value]-- != 0)
+            ++ret;
+        return ret;
+    }
+};

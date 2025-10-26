bf977e5784d3d6b14fcaac7941e3936ef8afa361
Madhesh H <144439699+madddx@users.noreply.github.com>
2025-10-26 12:57:57 +0530

    Create 2043. Simple Bank System.cpp

---
+++ 2001-2100/2043. Simple Bank System.cpp
@@ -0,0 +1,48 @@
+class Bank {
+    
+
+public:
+vector<long long> bal;
+  bool isValidAc(int account){
+     return account>=1 && account<=bal.size();
+  }
+    Bank(vector<long long>& balance) {
+       bal=balance;
+    }
+    
+    bool transfer(int account1, int account2, long long money) {
+        if(isValidAc(account1) && isValidAc(account2)){
+            if(bal[account1-1]>=money){
+                bal[account2-1]=bal[account2-1]+money;
+                bal[account1-1]=bal[account1-1]-money;
+                 return true;
+            }
+           
+        }
+        return false;
+    }
+    
+    bool deposit(int account, long long money) {
+        if(isValidAc(account)){
+            bal[account-1]+=money;
+            return true;
+        }
+        return false;
+    }
+    
+    bool withdraw(int account, long long money) {
+          if(isValidAc(account) && bal[account-1]>=money){
+            bal[account-1]-=money;
+            return true;
+    }
+    return false;
+    }
+};
+
+/**
+ * Your Bank object will be instantiated and called as such:
+ * Bank* obj = new Bank(balance);
+ * bool param_1 = obj->transfer(account1,account2,money);
+ * bool param_2 = obj->deposit(account,money);
+ * bool param_3 = obj->withdraw(account,money);
+ */

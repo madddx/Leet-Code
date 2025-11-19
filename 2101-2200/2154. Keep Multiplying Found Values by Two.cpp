f3626fa3b483da769bdc63b5d568fe51316c1c02
Madhesh H <144439699+madddx@users.noreply.github.com>
2025-11-19 09:54:06 +0530

    Create 2154. Keep Multiplying Found Values by Two.cpp

---
+++ 2101-2200/2154. Keep Multiplying Found Values by Two.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    int findFinalValue(vector<int>& nums, int original) {
+        bool foundValue = true;
+        int new_value = original;
+        while(foundValue) {
+            for(vector<int>::const_iterator it = nums.cbegin(); it != nums.cend(); it++) {
+                if(original == *it) {
+                    new_value = (original << 1);
+                    break;
+                }
+            }
+            foundValue = (new_value != original);
+            original = new_value;
+        }
+        return original;
+    }
+};

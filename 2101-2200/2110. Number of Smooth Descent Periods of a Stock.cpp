477b9158b1ea9386cdc1819685cbb12aeac41090
Madhesh H <144439699+madddx@users.noreply.github.com>
2025-12-15 16:55:28 +0530

    Create 2110. Number of Smooth Descent Periods of a Stock.cpp

---
+++ 2101-2200/2110. Number of Smooth Descent Periods of a Stock.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    long long getDescentPeriods(vector<int>& prices) {
+        
+        long long cnt = 0;
+        long long currStreak = 1;
+        long long prevPrice = -1;
+        for(int price : prices)
+        {
+            if(price > 0 && (prevPrice - price) == 1)
+            {
+                currStreak++;
+                cnt += currStreak;
+            }else
+            {
+                currStreak = 1;
+                cnt++;
+            }
+
+            // std::cout << prevPrice << " " << price << std::endl;
+            prevPrice = price;
+        }
+
+        return cnt;
+    }
+};

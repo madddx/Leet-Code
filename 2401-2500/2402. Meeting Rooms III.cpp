1d2bcbf302874d3be35fce0a179e2d7d1a356c52
Madhesh H <144439699+madddx@users.noreply.github.com>
2025-12-27 09:48:37 +0530

    Update 2402. Meeting Rooms III.cpp

--- 2401-2500/2402. Meeting Rooms III.cpp
+++ 2401-2500/2402. Meeting Rooms III.cpp
@@ -1,52 +1,55 @@
-const auto _ = std::cin.tie(nullptr)->sync_with_stdio(false);
-#define LC_HACK
-#ifdef LC_HACK
-const auto __ = []() {
-    struct ___ {
-        static void _() { std::ofstream("display_runtime.txt") << 0 << '\n'; }
-    };
-    std::atexit(&___::_);
-    return 0;
-}();
-#endif
-
 class Solution {
 public:
-    int mostBooked(int n, vector<vector<int>>& meetings) {
-        vector<int> rooms(n,0);
-        sort(meetings.begin(),meetings.end());
-        priority_queue<int,vector<int>,greater<int>> availablerooms;
-        priority_queue<pair<long long,int>,vector<pair<long long,int>>,greater<pair<long long,int>>> occupytime;
-        int a;
-        for(int i=0;i<n;i++){
-            availablerooms.push(i);
-        }
-        for(int i=0;i<meetings.size();i++){
-            while(!occupytime.empty() && occupytime.top().first <= meetings[i][0]){
-                availablerooms.push(occupytime.top().second);
-                occupytime.pop();
-            }
-            if(!availablerooms.empty()){
-                a=availablerooms.top();
-                availablerooms.pop();
-                rooms[a]++;
-                occupytime.push({meetings[i][1],a});
-            }
-            else{
-                pair<long long,int> b=occupytime.top();
-                occupytime.pop();
-                rooms[b.second]++;
-                occupytime.push({b.first+meetings[i][1]-meetings[i][0],b.second});
-            }
-        }
-        int max_bookings = -1;
-        int most_booked_room_idx = -1;
-        for (int i = 0; i < n; ++i) {
-            if (rooms[i] > max_bookings) {
-                max_bookings = rooms[i];
-                most_booked_room_idx = i;
-            }
-        }
-        return most_booked_room_idx;
+struct cmp
+{
+    bool operator()(const pair <long long, long long>& p1, const pair <long long, long long>& p2) const
+    {
+        if(p1.first != p2.first) return p1.first > p2.first;
+
+        return p1.second > p2.second;
     }
 };
+    int mostBooked(int n, vector<vector<int>>& meetings) {
+        sort(meetings.begin(), meetings.end());
+        priority_queue <int, vector <int>, greater <int> > rooms;
+        for(int i=0;i<n;i++) rooms.push(i);
+        vector <int> res(n);
+        priority_queue <pair <long long, int> , vector <pair <long long, int>>, greater <pair <long long, int>> > current;
+        int ans = 0, maxm = 0;
+        for(auto meeting: meetings)
+        {
+            while(!current.empty() && current.top().first <= meeting[0])
+            {
+                pair <long long, int> pp = current.top();
+                current.pop();
+                rooms.push(pp.second);
+            }
+            if(!rooms.empty())
+            {
+                current.push({meeting[1], rooms.top()});
+                res[rooms.top()]++;
+                rooms.pop();
+            }
+            else
+            {
+                pair <long long, int> pp = current.top();
+                current.pop();
+                res[pp.second]++;
+                current.push({pp.first+(meeting[1]-meeting[0]),pp.second});
+            }
+        }
+
+        for(int i=0;i<n;i++)
+        {
+            if(maxm < res[i])
+            {
+                maxm = res[i];
+                ans = i;
+            }
+        }
+
+        return ans;
+    }
+};
+
+auto init = atexit([](){ ofstream("display_runtime.txt") << "0"; });

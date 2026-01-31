e03a4e911efbb4e61b9f183ae7fbb65e4aa4e0b1
Madhesh H <144439699+madddx@users.noreply.github.com>
2026-01-31 18:11:59 +0530

    Create 744. Find Smallest Letter Greater Than Target.py

---
+++ 701-800/744. Find Smallest Letter Greater Than Target.py
@@ -0,0 +1,16 @@
+class Solution:
+    def nextGreatestLetter(self, letters, target):
+        low = 0
+        high = len(letters) - 1
+        ans = 0
+
+        while low <= high:
+            mid = (low + high) // 2
+
+            if letters[mid] > target:
+                ans = mid
+                high = mid - 1
+            else:
+                low = mid + 1
+
+        return letters[ans]
